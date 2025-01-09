bc51c48acd32f326e97cab804590f57e2ecebd61
Daniil Poletaev <44584010+danpoletaev@users.noreply.github.com>
2025-01-09 17:17:12 +0100

    feat: test redirect endpoints

--- src/main.ts
+++ src/main.ts
@@ -5,34 +5,29 @@ import { Actor } from 'apify';
 // The init() call configures the Actor for its environment. It's recommended to start every Actor with an init()
 await Actor.init();
 
-// A simple utility function to parse the request body.
-const parseBody = async (req: http.IncomingMessage) => {
-    return new Promise((resolve, reject) => {
-        let body: Buffer[] = [];
-        req
-            .on('data', (chunk: Buffer) => {
-                body.push(chunk);
-            })
-            .on('end', () => {
-                resolve(Buffer.concat(body).toString());
-            })
-            .on('error', (err: unknown) => {
-                reject(err);
-            });
-    });
-};
 
 // A simple HTTP server that responds with the request headers, method, body, and query parameters.
 const server = http.createServer(async (req, res) => {
     const { headers, method, url } = req;
-    const body = await parseBody(req);
+
+    if (url!.startsWith('/cookies/set')) {
+        res.writeHead(301, { Location: '/cookies' });
+        res.end();
+        return;
+    }
+
+    if (url!.startsWith('/cookies')) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({
+            message: "Successfully reached destination",
+        }));
+        return;
+    }
+
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({
-        headers,
-        method,
-        body: body || undefined,
-        queryParams: Object.fromEntries(new URLSearchParams(url?.split('?')[1] || '')),
-    }));
+        res.end(JSON.stringify({
+            message: "Default route",
+        }));
 });
 
 // Listen on the Actor Standby port
